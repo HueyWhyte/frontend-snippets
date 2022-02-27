90eb19f13ef742b6e8ec3228ba0572cd3ece92ff
Stanley Kwamina Otabil <78874225+KwaminaOtabil@users.noreply.github.com>
2022-02-27 20:41:45 +0000

    first commit

--- app/routes/index.js
+++
@@ -1,8 +0,0 @@
-import Chat from "./Chat";
-import Twitter from "./Twitter";
-import Message from "./Message";
-import ArtLanding from "./ArtLanding";
-import CryptoDashboard from "./CryptoDashboard";
-import Bet from "./Bet";
-
-export { Twitter, Chat, Message, ArtLanding, CryptoDashboard, Bet };
